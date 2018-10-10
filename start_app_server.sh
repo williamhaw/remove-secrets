6a6c7a70f56a6eb33575f855d1087e6a67f02c31
William Haw <william.haw@agoda.com>
2018-10-10 19:54:14 +0800

    Add hardcoded mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://<dbuser>:<dbpassword>@ds227853.mlab.com:27853/mydb
 
 # finally, run the app in dev mode...
 npm run dev
